e3303e95651a3b0f275f44b2399fdc4d4ba42e63
ffmcgee <joao.carlos@consensys.net>
2024-12-16 14:35:07 +0100

    docs: slight docs update

--- src/helpers/AddressHelpers.ts
+++ src/helpers/AddressHelpers.ts
@@ -1,7 +1,7 @@
 /**
- * Formats addresses with respective scope to create a session. See [CAIP-25](https://github.com/ChainAgnostic/CAIPs/blob/main/CAIPs/caip-25.md).
+ * Formats addresses as [CAIP-10](https://chainagnostic.org/CAIPs/caip-10) addresses for it's respective request scope. See [CAIP-25](https://github.com/ChainAgnostic/CAIPs/blob/main/CAIPs/caip-25.md) and .
  * @param scope - The scope to create session for.
- * @param addresses - The addresses to create session for. If address is empty, we remove it from the array.
+ * @param addresses - The addresses to format. If address is empty, we remove it from the array.
  * @returns The formatted addresses with the scope to create session for.
  */
 export const getCreateSessionOptionalScopesFormattedAddresses = (
