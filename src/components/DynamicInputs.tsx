14902f96e3c6c5401656cc29e7f7361b3b8b5ee2
ffmcgee <joao.carlos@consensys.net>
2024-12-20 16:20:45 +0100

    feat: multi input for custom scopes

--- src/components/DynamicInputs.tsx
+++ src/components/DynamicInputs.tsx
@@ -1,13 +1,35 @@
 import React, { useCallback } from 'react';
 
+export enum INPUT_LABEL_TYPE {
+  ADDRESS = 'Address',
+  SCOPE = 'Custom',
+}
+
+const LABEL_PLACEHOLDER = {
+  [INPUT_LABEL_TYPE.ADDRESS]: '0x483b...5f97',
+  [INPUT_LABEL_TYPE.SCOPE]: 'eip155:1',
+};
+
+const INPUT_ID = {
+  [INPUT_LABEL_TYPE.ADDRESS]: 'custom-address-input',
+  [INPUT_LABEL_TYPE.SCOPE]: 'custom-scope-input',
+};
+
+const ADD_BUTTON_ID = {
+  [INPUT_LABEL_TYPE.ADDRESS]: 'add-custom-address-button',
+  [INPUT_LABEL_TYPE.SCOPE]: 'add-custom-scope-button',
+};
+
 type DynamicInputsProps = {
   inputArray: string[];
   setInputArray: React.Dispatch<React.SetStateAction<string[]>>;
+  label: INPUT_LABEL_TYPE;
 };
 
-const DynamicAddressInputs: React.FC<DynamicInputsProps> = ({
+const DynamicInputs: React.FC<DynamicInputsProps> = ({
   inputArray,
   setInputArray,
+  label,
 }) => {
   const handleInputChange = useCallback(
     (index: number, value: string) => {
@@ -29,18 +51,23 @@ const DynamicAddressInputs: React.FC<DynamicInputsProps> = ({
       {inputArray.map((input, index) => (
         <div key={index}>
           <label>
-            Address:
+            {label}:
             <input
+              id={`${INPUT_ID[label]}-${index}`}
               type="text"
               value={input}
               onChange={(inputEvent) =>
                 handleInputChange(index, inputEvent.target.value)
               }
-              placeholder="0x483b...5f97"
+              placeholder={LABEL_PLACEHOLDER[label]}
             />
           </label>
           {index === inputArray.length - 1 && inputArray.length < 5 && (
-            <button onClick={addInput} disabled={!input}>
+            <button
+              id={`${ADD_BUTTON_ID[label]}-${index}`}
+              onClick={addInput}
+              disabled={!input}
+            >
               +
             </button>
           )}
@@ -50,4 +77,4 @@ const DynamicAddressInputs: React.FC<DynamicInputsProps> = ({
   );
 };
 
-export default DynamicAddressInputs;
+export default DynamicInputs;
